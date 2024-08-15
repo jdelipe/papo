b535b20724d6930ab792851d101eb1eaa00f3de4
jdelipe <lgaranhuns@gmail.com>
2024-08-14 21:13:12 -0300

    Add files via upload

---
+++ seupapo.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("image-container").style.display = "flex";
+    this.style.display = "none";  // Faz o botão desaparecer
+});
